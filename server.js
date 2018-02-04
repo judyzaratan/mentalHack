5fe6bc668d6428d15928646a52003937956bf146
simonxca <simoncxiong@gmail.com>
2018-02-03 20:50:38 -0800

    Add gate questions page

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname + '/index.html'))
 );
 
+app.get('/gateQuestions', (req, res) =>
+    res.sendFile(path.join(__dirname + '/gateQuestions.html'))
+);
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
