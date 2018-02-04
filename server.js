84915c829ec8632fcf7d3573780e2df5638145d9
judyzaratan <judy.zaratan@gmail.com>
2018-02-04 10:43:44 -0800

    add more screening page features

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express()
 
+
+app.use('/assets', express.static(`${__dirname}/assets`));
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname + '/screening.html'))
 );
