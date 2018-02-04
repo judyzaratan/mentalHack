35e8469e0d0a607d7e49644d135c1abd553cc421
judyzaratan <judy.zaratan@gmail.com>
2018-02-04 10:00:01 -0800

    screening page updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express()
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname + '/index.html'))
+    res.sendFile(path.join(__dirname + '/screening.html'))
 );
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
