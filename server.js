4516b492087bc94744017941f0e1179a5949ea10
simonxca <simoncxiong@gmail.com>
2018-02-03 21:50:53 -0800

    Add basic landing page

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 const app = express()
 
+app.use('/assets', express.static(`${__dirname}/assets`));
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname + '/index.html'))
 );
